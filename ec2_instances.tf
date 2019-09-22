e156222853717ed342a95918e2b7b1b9cfbc7916
karolnedza <knedza@OSX-10.10-BASE>
2019-09-22 18:44:34 +0200

    Corrected ec2 ami id

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web-rvbd" {
   count                  = "${var.instance_count}"
-  ami                    = "${data.aws_ami.ami_image.name}"
+  ami                    = "${data.aws_ami.ami_image.id}"
   instance_type          = "t2.micro"
   subnet_id              = "${element(aws_subnet.subnet.*.id,count.index % var.subnet_count)}"
   vpc_security_group_ids = ["${aws_security_group.DMZ.id}"]
